6e253e127cb60db85a61721b88c5ed0290cb6e12
HelgaUa <Elgaua@gmail.com>
2023-09-18 22:54:28 +0300

    Data types added

---
+++ main.js
@@ -0,0 +1,33 @@
+let hours = 24;
+console.log(typeof hours);
+
+let city = 'kharkiv';
+console.log(typeof city);
+
+let seconds = 240989878787987879878979878798798777n;
+console.log(typeof seconds);
+
+let signed = true;
+let notSigned = false;
+console.log(typeof signed, typeof notSigned);
+
+const cell = null;
+console.log(cell);
+console.log(typeof cell);
+
+let secondName;
+console.log(typeof secondName);
+
+const password = Symbol("description");
+console.log(typeof password);
+
+let product = {
+    name: 'dress',
+    size: 'S',
+    color: 'black'
+}
+console.log(typeof product);
+
+
+
+
